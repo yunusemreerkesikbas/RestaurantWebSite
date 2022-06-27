f3ad8f97343f08b847142a6b51c9b5fa16347fab
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-06-27 19:19:38 +0300

    refactoring navbar and footer added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 let navbar = document.querySelector('.header .flex .navbar');
+let profile = document.querySelector('.header .flex .profile');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    
+    profile.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    profile.classList.remove('active');
 }
